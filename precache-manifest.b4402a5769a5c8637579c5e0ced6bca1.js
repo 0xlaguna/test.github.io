f7e5ef4c65875da3396a09aee572c6c0ac3984c9
Administrator <lagunamichael2@outlook.com>
2019-09-20 11:39:03 -0600

    Updates

---
+++ precache-manifest.b4402a5769a5c8637579c5e0ced6bca1.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a9ff46454bea9ca3d89183b06b0f6ed9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3ead0298ec5979f8a28e",
+    "url": "/static/css/main.df987d48.chunk.css"
+  },
+  {
+    "revision": "3bd6142965949b9e1a17",
+    "url": "/static/js/2.e3d677df.chunk.js"
+  },
+  {
+    "revision": "3ead0298ec5979f8a28e",
+    "url": "/static/js/main.c221f766.chunk.js"
+  },
+  {
+    "revision": "8b304daa83f829d6961a",
+    "url": "/static/js/runtime~main.af398c66.js"
+  },
+  {
+    "revision": "2f48abc8c45de9add264c5be432e878e",
+    "url": "/static/media/HACKED.2f48abc8.ttf"
+  },
+  {
+    "revision": "4d55fdbc858e2331cde88111efea3db9",
+    "url": "/static/media/Industry-Book.4d55fdbc.woff2"
+  },
+  {
+    "revision": "a54d01c7602398399d9370f79363e128",
+    "url": "/static/media/Industry-Book.a54d01c7.woff"
+  },
+  {
+    "revision": "92e16888d418d46366f85f07d536ef25",
+    "url": "/static/media/mejia1cc.92e16888.png"
+  },
+  {
+    "revision": "63bfa20b6e5658bff2c8236b52933261",
+    "url": "/static/media/serial.63bfa20b.gif"
+  },
+  {
+    "revision": "626d94156903f7181a4e5e07e2ef5bae",
+    "url": "/static/media/uni1.626d9415.png"
+  }
+]);
